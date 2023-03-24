6a0bd493bac0e3323f397b07a3f11ebac7b8ec92
Aubrey Polderman <aubreypolderman@zlm.nl>
2023-03-25 00:01:11 +0100

    Add project files.

---
+++ ZLMClaims/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using ZLMClaims.Auth0;
+
+namespace ZLMClaims;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+        builder.Services.AddSingleton<MainPage>();
+
+		// Auth0 registration
+        builder.Services.AddSingleton(new Auth0Client(new()
+        {
+            Domain = "dev-ajcve7wvqiq10doi.eu.auth0.com",
+            ClientId = "kgYGjeSFKI7GczXNx335myCMXbdwx6UG",
+            Scope = "openid profile",
+            RedirectUri = "myapp://callback"
+        }));
+
+        return builder.Build();
+	}
+}
