75e5c91ccfd480227fded346808762aabde44029
Aubrey Polderman <aubreypolderman@zlm.nl>
2023-04-17 08:00:13 +0200

    Code clean userviewmodel

--- ZLMClaims/ViewModels/UserViewModel.cs
+++ ZLMClaims/ViewModels/UserViewModel.cs
@@ -13,29 +13,29 @@ using ZLMClaims.Resources.Languages;
 using ZLMClaims.Services;
 public class UserViewModel : INotifyPropertyChanged
 {
-      
+
     private User _user;
-        public User User
-        {
-        
+    public User User
+    {
+
         get { return _user; }
-            set
-            {
-                _user = value;
-                OnPropertyChanged(nameof(User));
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName)
+        set
         {
+            _user = value;
+            OnPropertyChanged(nameof(User));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
         Console.WriteLine("[UserViewModel] [OnPropertyChanged] [==============] start with propertyname " + propertyName);
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+    }
 
-        public async Task GetUser(int userId)
-        {
+    public async Task GetUser(int userId)
+    {
         Debug.WriteLine("[UserViewModel] [GetUser] [==============] Debug GetUser with id " + userId);
         Console.WriteLine("[UserViewModel] [GetUser] [==============] GetUser with id " + userId);
         HttpClient client = new HttpClient();
@@ -45,7 +45,7 @@ public class UserViewModel : INotifyPropertyChanged
         Console.WriteLine("[UserViewModel] [GetUser] [==============] reponse 2 " + response);
         string json = await response.Content.ReadAsStringAsync();
         User = Newtonsoft.Json.JsonConvert.DeserializeObject<User>(json);
-        }
+    }
 
     public void OnThemeSwitchToggled()
     {
--- ZLMClaims/Views/UserPage.xaml.cs
+++ ZLMClaims/Views/UserPage.xaml.cs
@@ -10,40 +10,22 @@ public partial class UserPage : ContentPage
 
     public UserPage() 
     {
-        Console.WriteLine("[UserPage] [noargs constructor] [==============] Start");
-        Console.WriteLine("[UserPage] [noargs constructor] [==============] Before InitializeComponent");
         InitializeComponent();
-        Console.WriteLine("[UserPage] [noargs constructor] [==============] After InitializeComponent");
 
-        Console.WriteLine("[UserPage] [noargs constructor] [==============] Before instantiation of UserViewModel");
         _viewModel = new UserViewModel();
-        Console.WriteLine("[UserPage] [noargs constructor] [==============] After instantiation of UserViewModel");
-        Console.WriteLine("[UserPage] [noargs constructor] [==============] Before call GetUser with hardcoded id 1");
-        _viewModel.GetUser(1);
-        Console.WriteLine("[UserPage] [noargs constructor] [==============] After call GetUser with hardcoded id 1");
-
-        Console.WriteLine("[UserPage] [noargs constructor] [==============] Before bindingContext with _viewmodel");
+               _viewModel.GetUser(9);
+       
         BindingContext = _viewModel;
-        Console.WriteLine("[UserPage] [noargs constructor] [==============] After bindingContext with _viewmodel");
     }
 
     public UserPage(int userId)
     {
         Console.WriteLine("[UserPage] [constructor] [==============] Start with userId " + userId);
-        Console.WriteLine("[UserPage] [constructor] [==============] Before InitializeComponent");
         InitializeComponent();
-        Console.WriteLine("[UserPage] [constructor] [==============] After InitializeComponent");
-
-        Console.WriteLine("[UserPage] [constructor] [==============] Before instantiation of UserViewModel");
         _viewModel = new UserViewModel();
-        Console.WriteLine("[UserPage] [constructor] [==============] After instantiation of UserViewModel");
-        Console.WriteLine("[UserPage] [constructor] [==============] Before call GetUser with id " + userId);
         _viewModel.GetUser(userId);
-        Console.WriteLine("[UserPage] [constructor] [==============] After call GetUser with id " + userId);
-
-        Console.WriteLine("[UserPage] [constructor] [==============] Before bindingContext with _viewmodel");
         BindingContext = _viewModel;
-        Console.WriteLine("[UserPage] [constructor] [==============] After bindingContext with _viewmodel");
+        
     }
 
     public LocalizationResourceManager LocalizationResourceManager
