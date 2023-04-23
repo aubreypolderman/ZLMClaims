6f28968ff0b2087549cb7f043bb129167f7c3c92
aubrey <aubrey@Z04553>
2023-04-23 12:44:48 +0200

    Added google maps config and key.

--- ZLMClaims/MauiProgram.cs
+++ ZLMClaims/MauiProgram.cs
@@ -22,7 +22,8 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+            })
+            .UseMauiMaps();
 
 #if DEBUG
         builder.Logging.AddDebug();
--- ZLMClaims/Views/AllClaimsPage.xaml.cs
+++ ZLMClaims/Views/AllClaimsPage.xaml.cs
@@ -6,16 +6,10 @@ public partial class AllClaimsPage : ContentPage
 {
 
     public AllClaimsPage(AllClaimsViewModel vm)
-    {
-        Console.WriteLine("[..............] [AllClaimsPage] [AllClaimsViewModel] viewmodel injected");
+	{
+        Console.WriteLine("[..............] [AllClaimsPage] [AllContractsViewModel] viewmodel injected");
         BindingContext = vm;
         InitializeComponent();
- 
-    }
-
-    private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
-    {
-        claimsCollection.SelectedItem = null;
     }
 
     private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
@@ -24,15 +18,15 @@ public partial class AllClaimsPage : ContentPage
         var claim = ((VisualElement)sender).BindingContext as Claim;
         if (claim == null)
         {
-            Console.WriteLine("[..............] [AllClaimsPage] [TapGestureRecognizer_Tapped] Contract is null. Return");
+            Console.WriteLine("[..............] [AllClaimsPage] [TapGestureRecognizer_Tapped] Claim is null. Return");
             return;
         }
-        Console.WriteLine("[..............] [AllClaimsPage] [TapGestureRecognizer_Tapped] Contract: " + claim);
+        Console.WriteLine("[..............] [AllClaimsPage] [TapGestureRecognizer_Tapped] Claim: " + claim);
 
         // Navigate to detail page of contract
-        await Shell.Current.GoToAsync(nameof(AllClaimsPage), true, new Dictionary<string, object>
+        await Shell.Current.GoToAsync(nameof(ClaimFormStep1Page), true, new Dictionary<string, object>
         {
-            {nameof(Claim), claim}
+            {nameof(ClaimFormStep1Page), claim}
         });
     }
 }
