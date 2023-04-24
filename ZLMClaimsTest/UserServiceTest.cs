4547e83c9e1f93a2190df114edc404128a1e8534
aubrey <aubrey@Z04553>
2023-04-24 23:16:03 +0200

    added unit test service

---
+++ ZLMClaimsTest/UserServiceTest.cs
@@ -0,0 +1,22 @@
+using Xunit;
+namespace ZLMClaimsTest
+{
+    public class UserServiceTest
+    {
+        [Fact]
+        public async Task GetUserById()
+        {
+            // arrange
+            var userService = Substitute.For<IUserService>();
+            var userId = 1;
+
+            // act
+            var user = await userService.GetUserByIdAsync(userId);
+
+            // assert
+            Assert.NotNull(user);
+            Assert.Equal(userId, user.Id);
+
+        }
+    }
+}
