fe4523d96b0d7a2ab9086eef5336776da68df2f4
aubreypolderman <92875359+aubreypolderman@users.noreply.github.com>
2023-05-31 14:11:55 +0200

    Made fields of ClaimForm5Page more flexible

--- ZLMClaims/Auth0/Auth0Client.cs
+++ ZLMClaims/Auth0/Auth0Client.cs
@@ -3,104 +3,103 @@ using IdentityModel.OidcClient.Browser;
 using IdentityModel.Client;
 using System.Security.Claims;
 
-namespace ZLMClaims.Auth0
+namespace ZLMClaims.Auth0;
+
+public class Auth0Client
 {
-    public class Auth0Client
+    private readonly OidcClient oidcClient;
+
+    public Auth0Client(Auth0ClientOptions options)
     {
-        private readonly OidcClient oidcClient;
-
-        public Auth0Client(Auth0ClientOptions options)
+        oidcClient = new OidcClient(new OidcClientOptions
         {
-            oidcClient = new OidcClient(new OidcClientOptions
-            {
-                Authority = $"https://{options.Domain}",
-                ClientId = options.ClientId,
-                Scope = options.Scope,
-                RedirectUri = options.RedirectUri,
-                Browser = options.Browser
-            });
+            Authority = $"https://{options.Domain}",
+            ClientId = options.ClientId,
+            Scope = options.Scope,
+            RedirectUri = options.RedirectUri,
+            Browser = options.Browser
+        });
+    }
+
+    public IdentityModel.OidcClient.Browser.IBrowser Browser
+    {
+        get
+        {
+            return oidcClient.Options.Browser;
+        }
+        set
+        {
+            oidcClient.Options.Browser = value;
+        }
+    }
+
+    public async Task<LoginResult> LoginAsync()
+    {
+        Console.WriteLine(DateTime.Now + "[..............] [Auth0Client] [LoginAsync] voor invoke oidcClient.LoginAsync");
+        var loginResult = await oidcClient.LoginAsync();
+        Console.WriteLine(DateTime.Now + "[..............] [Auth0Client] [LoginAsync] na invoke oidcClient.LoginAsync met result " + loginResult);
+        Console.WriteLine(DateTime.Now + "[..............] [Auth0Client] [LoginAsync] na invoke oidcClient.LoginAsync AccessToken " + loginResult.AccessToken);
+        Console.WriteLine(DateTime.Now + "[..............] [Auth0Client] [LoginAsync] na invoke oidcClient.LoginAsync IdentityToken " + loginResult.IdentityToken);
+
+        if (!loginResult.IsError)
+        {
+            await SecureStorage.Default.SetAsync("access_token", loginResult.AccessToken);
+            await SecureStorage.Default.SetAsync("id_token", loginResult.IdentityToken);
         }
 
-        public IdentityModel.OidcClient.Browser.IBrowser Browser
+        return loginResult;
+    }
+
+    // logout
+    public async Task<BrowserResult> LogoutAsync()
+    {
+        var logoutParameters = new Dictionary<string, string>
         {
-            get
-            {
-                return oidcClient.Options.Browser;
-            }
-            set
-            {
-                oidcClient.Options.Browser = value;
-            }
-        }
+            {"client_id", oidcClient.Options.ClientId },
+            {"returnTo", oidcClient.Options.RedirectUri }
+        };
 
-        public async Task<LoginResult> LoginAsync()
+        var logoutRequest = new LogoutRequest();
+        var endSessionUrl = new RequestUrl($"{oidcClient.Options.Authority}/v2/logout")
+          .Create(new Parameters(logoutParameters));
+        var browserOptions = new BrowserOptions(endSessionUrl, oidcClient.Options.RedirectUri)
         {
-            Console.WriteLine(DateTime.Now + "[..............] [Auth0Client] [LoginAsync] voor invoke oidcClient.LoginAsync");
-            var loginResult = await oidcClient.LoginAsync();
-            Console.WriteLine(DateTime.Now + "[..............] [Auth0Client] [LoginAsync] na invoke oidcClient.LoginAsync met result " + loginResult);
-            Console.WriteLine(DateTime.Now + "[..............] [Auth0Client] [LoginAsync] na invoke oidcClient.LoginAsync AccessToken " + loginResult.AccessToken);
-            Console.WriteLine(DateTime.Now + "[..............] [Auth0Client] [LoginAsync] na invoke oidcClient.LoginAsync IdentityToken " + loginResult.IdentityToken);
+            Timeout = TimeSpan.FromSeconds(logoutRequest.BrowserTimeout),
+            DisplayMode = logoutRequest.BrowserDisplayMode
+        };
 
-            if (!loginResult.IsError)
-            {
-                await SecureStorage.Default.SetAsync("access_token", loginResult.AccessToken);
-                await SecureStorage.Default.SetAsync("id_token", loginResult.IdentityToken);
-            }
+        var browserResult = await oidcClient.Options.Browser.InvokeAsync(browserOptions);
 
-            return loginResult;
-        }
+        SecureStorage.Default.RemoveAll();
 
-        // logout
-        public async Task<BrowserResult> LogoutAsync()
+        return browserResult;
+    }
+
+    public async Task<ClaimsPrincipal> GetAuthenticatedUser()
+    {
+        ClaimsPrincipal user = null;
+
+        var idToken = await SecureStorage.Default.GetAsync("id_token");
+
+        if (idToken != null)
         {
-            var logoutParameters = new Dictionary<string, string>
+            var doc = await new HttpClient().GetDiscoveryDocumentAsync(oidcClient.Options.Authority);
+            var validator = new JwtHandlerIdentityTokenValidator();
+            var options = new OidcClientOptions
             {
-                {"client_id", oidcClient.Options.ClientId },
-                {"returnTo", oidcClient.Options.RedirectUri }
-            };
-
-            var logoutRequest = new LogoutRequest();
-            var endSessionUrl = new RequestUrl($"{oidcClient.Options.Authority}/v2/logout")
-              .Create(new Parameters(logoutParameters));
-            var browserOptions = new BrowserOptions(endSessionUrl, oidcClient.Options.RedirectUri)
-            {
-                Timeout = TimeSpan.FromSeconds(logoutRequest.BrowserTimeout),
-                DisplayMode = logoutRequest.BrowserDisplayMode
-            };
-
-            var browserResult = await oidcClient.Options.Browser.InvokeAsync(browserOptions);
-
-            SecureStorage.Default.RemoveAll();
-
-            return browserResult;
-        }
-
-        public async Task<ClaimsPrincipal> GetAuthenticatedUser()
-        {
-            ClaimsPrincipal user = null;
-
-            var idToken = await SecureStorage.Default.GetAsync("id_token");
-
-            if (idToken != null)
-            {
-                var doc = await new HttpClient().GetDiscoveryDocumentAsync(oidcClient.Options.Authority);
-                var validator = new JwtHandlerIdentityTokenValidator();
-                var options = new OidcClientOptions
+                ClientId = oidcClient.Options.ClientId,
+                ProviderInformation = new ProviderInformation
                 {
-                    ClientId = oidcClient.Options.ClientId,
-                    ProviderInformation = new ProviderInformation
-                    {
-                        IssuerName = doc.Issuer,
-                        KeySet = doc.KeySet
-                    }
-                };
+                    IssuerName = doc.Issuer,
+                    KeySet = doc.KeySet
+                }
+            };
 
-                var validationResult = await validator.ValidateAsync(idToken, options);
+            var validationResult = await validator.ValidateAsync(idToken, options);
 
-                if (!validationResult.IsError) user = validationResult.User;
-            }
-
-            return user;
+            if (!validationResult.IsError) user = validationResult.User;
         }
+
+        return user;
     }
 }
